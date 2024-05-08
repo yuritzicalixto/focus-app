11c2e1476b991c8395602c3633b7e4546fdfee6e
yuritzicalixto <ramirezcalixtodiana@gmail.com>
2024-05-07 19:21:37 -0600

    Aplicando audio y estilos

--- main.js
+++ main.js
@@ -3,21 +3,40 @@ const botonCorto = document.querySelector('.app__card-button--corto');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
-const titulo = document.querySelector('.app__title')
+const titulo = document.querySelector('.app__title');
+const botones = document.querySelectorAll('.app__card-button');
+const inputEnfoqueMusica = document.querySelector('#alternar-musica');
+const musica=new Audio('./sonidos/luna-rise-part-one.mp3');
+
+inputEnfoqueMusica.addEventListener('change', ()=>{
+    if(musica.paused){
+        musica.play()
+    } else {
+        musica.pause()
+    }
+});
 
 botonCorto.addEventListener('click', ()=> {
     cambiarContexto('descanso-corto');
+    botonCorto.classList.add('active');
 });
 
 botonEnfoque.addEventListener('click', ()=> {
     cambiarContexto('enfoque');
+    botonEnfoque.classList.add('active');
 });
 
 botonLargo.addEventListener('click', ()=> {
     cambiarContexto('descanso-largo');
+    botonLargo.classList.add('active');
 });
 
 function cambiarContexto(contexto){
+
+    botones.forEach(function(contexto){
+        contexto.classList.remove('active');
+    });
+
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `./imagenes/${contexto}.png`);
 
